bc86bcb34794d4a7107d6783831f89332564d21d
jyothisreesajan <jyothisreesajan@gmail.com>
2025-09-25 22:29:07 +0530

    Started with flask

--- ai_backend/app.py
+++ ai_backend/app.py
@@ -1,19 +1,28 @@
+from flask import Flask, request
 from ultralytics import YOLO
+import  numpy as np
 import cv2
 
+app = Flask(__name__)
 model = YOLO('models/yolov8n.pt')
-
-image_path = 'sample.jpg'
-img = cv2.imread(image_path)
-
-if img is None:
-    print("Error: Could not load image")
-else:
+@app.route('/detect', methods=['POST'])
+def detect():
+    if 'image' not in request.files:
+        print("Error: No image provided")
+        return "No image provided", 400
     
+    file = request.files['image']
+    img_array = np.frombuffer(file.read(), np.uint8)
+    img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+
+    if img is None:
+        print("Error: Could not load image")
     results = model(img)
     annotated_img = results[0].plot()
 
-    
     cv2.imshow("YOLOv8 Detection", annotated_img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001)
--- ui_backend/app.py
+++ ui_backend/app.py
@@ -1,1 +1,16 @@
+from flask import Flask, request, jsonify
 
+app = Flask(__name__)
+
+@app.route('/upload', methods=['GET'])
+def upload():
+    if 'image' not in request.files:
+        return jsonify({'error': 'No image provided'}), 400
+    
+    file = request.files['image']
+    files = {'image': file}
+    response = requests.post('http://ai_backend:5001/detect', files=files)
+    
+    return response.text, response.status_code
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
